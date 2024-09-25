3dfd85dc802585f68b193e727fe2e816b1e0f55d
Matthew <matthewhannemann@yahoo.com>
2024-09-25 12:29:54 -0700

    Add files via upload

---
+++ Advanced JavaScript Projects/JavaScript/JavaScript_3.js
@@ -0,0 +1,5 @@
+function displayType(family){
+    var familyType = family.getAttribute("data-faimly-type");
+        alert(familyType + " is part of the " + family.innerHTML + " family!");
+    
+}
